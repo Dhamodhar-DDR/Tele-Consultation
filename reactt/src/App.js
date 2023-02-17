4e6fdfcc238eeff8491f2dffdf27f38ec94bb64c
chandrahassai77 <chandrahaskotagiri@gmail.com>
2023-02-17 22:08:53 +0530

    first-commit

---
+++ reactt/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Login from './Login';
+
+
+function App() 
+{
+  const tst = "Test Text";
+  return (
+    
+     <>
+
+     <div className='pg1'>
+      <Login />
+     </div>
+
+
+      </>
+    
+  );
+}
+export default App;
